6b32d230814017e90aba82be704caef697833bee
Andy Dam <andy@andydam.com>
2017-12-13 13:20:04 -0800

    added search youtube options, completed all basic spec tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,8 +1,20 @@
 var searchYouTube = (options, callback) => {
+  //set default options if none were passed
+  options.key = options.key || window.YOUTUBE_API_KEY;
+  options.query = options.query || 'cats';
+  options.max = options.max || 5;
+
   //use jquery get to list of youtube videos
   //when call is sucessful, call the callback, passing only the items array of videos to the callback
   $.get('https://www.googleapis.com/youtube/v3/search',
-    {key: window.YOUTUBE_API_KEY, q: options.query, part: 'snippet', maxResults: 5},
+    {
+      key: options.key,
+      q: options.query,
+      part: 'snippet',
+      maxResults: options.max,
+      type: 'video',
+      videoEmbeddable: 'true'
+    },
     data => callback(data.items));
 };
 
